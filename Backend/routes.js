80e0ccecf3137b8f6332bee466727d4390d84371
Dayber Alfonso <0036161sm@gmail.com>
2024-04-11 15:22:57 -0400

    Update routes.js

--- Backend/routes.js
+++ Backend/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const path = require('path')
 
 router.use(express.static(path.join(__dirname, '../Mash/dist')))
+//Handling user login
 router.post('/login', async (req, res) => {
     const {Username, Password } = req.body;
     console.log(req.body)
