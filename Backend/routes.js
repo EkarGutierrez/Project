34a7c0115e5f2db760289fe7531afa00e4e3a8ca
Dayber Alfonso <0036161sm@gmail.com>
2024-04-11 01:02:48 -0400

    Created server with initial endpoint /login
    Hosting frontend artifacts

---
+++ Backend/routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const User = require('./user.model');
+const router = express.Router();
+const path = require('path')
+
+router.use(express.static(path.join(__dirname, '../Mash/dist')))
+router.post('/login', async (req, res) => {
+    const {Username, Password } = req.body;
+    console.log(req.body)
+    try {
+        // Check if user exists
+        const user = await User.findOne({Username }).exec();
+        console.log(user)
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        // Check if password is correct
+        if (Password !== user.Password) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        // Authentication successful
+        res.json({ message: 'Login successful', user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+module.exports = router;
---
+++ Backend/user.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    Username: { type: String, required: true },
+    Password: { type: String, required: true },
+    Email: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema, 'UserLoginInfo');
+
+module.exports = User;
